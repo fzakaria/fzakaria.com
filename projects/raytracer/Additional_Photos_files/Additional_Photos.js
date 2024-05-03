58ca538b3ea3afe0a841c281dd08cc6e73e77edc
Farid Zakaria <fmzakari@google.com>
2024-05-03 21:47:25 +0000

    Site updated at 2024-05-03 21:47:24 UTC

---
+++ projects/raytracer/Additional_Photos_files/Additional_Photos.js
@@ -0,0 +1,29 @@
+// Created by iWeb 3.0.1 local-build-20100721
+
+function createMediaStream_id2()
+{return IWCreatePhotocast("file://localhost/Users/Setheron/Sites/raytracer/RayTracing/Additional_Photos_files/rss.xml",true);}
+function initializeMediaStream_id2()
+{createMediaStream_id2().load('file://localhost/Users/Setheron/Sites/raytracer/RayTracing',function(imageStream)
+{var entryCount=imageStream.length;var headerView=widgets['widget1'];headerView.setPreferenceForKey(imageStream.length,'entryCount');NotificationCenter.postNotification(new IWNotification('SetPage','id2',{pageIndex:0}));});}
+function layoutMediaGrid_id2(range)
+{createMediaStream_id2().load('file://localhost/Users/Setheron/Sites/raytracer/RayTracing',function(imageStream)
+{if(range==null)
+{range=new IWRange(0,imageStream.length);}
+IWLayoutPhotoGrid('id2',new IWPhotoGridLayout(4,new IWSize(102,102),new IWSize(102,28),new IWSize(122,145),27,27,0,new IWSize(0,0)),new IWEmptyStroke(),imageStream,range,null,null,1.000000,{backgroundColor:'rgb(0, 0, 0)',reflectionHeight:100,reflectionOffset:2,captionHeight:100,fullScreen:0,transitionIndex:2},'Media/slideshow.html','widget1','widget2','widget3')});}
+function relayoutMediaGrid_id2(notification)
+{var userInfo=notification.userInfo();var range=userInfo['range'];layoutMediaGrid_id2(range);}
+function onStubPage()
+{var args=window.location.href.toQueryParams();parent.IWMediaStreamPhotoPageSetMediaStream(createMediaStream_id2(),args.id);}
+if(window.stubPage)
+{onStubPage();}
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{IWRegisterNamedImage('comment overlay','Media/Photo-Overlay-Comment.png')
+IWRegisterNamedImage('movie overlay','Media/Photo-Overlay-Movie.png')
+loadMozillaCSS('Additional_Photos_files/Additional_PhotosMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');NotificationCenter.addObserver(null,relayoutMediaGrid_id2,'RangeChanged','id2')
+adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');initializeMediaStream_id2()
+performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
