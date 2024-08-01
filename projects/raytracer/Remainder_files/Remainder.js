0df9f36db632577e4de85e60d257f29d80b55490
Farid Zakaria <farid.m.zakaria@gmail.com>
2024-07-31 22:50:05 -0700

    Site updated at 2024-08-01 05:50:05 UTC

---
+++ projects/raytracer/Remainder_files/Remainder.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.1 local-build-20100721
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Remainder_files/RemainderMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');Widget.onload();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
