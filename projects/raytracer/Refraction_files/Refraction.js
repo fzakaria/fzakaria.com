40225f7b9644e537dea6bc9d72f0ef69932281bb
Farid Zakaria <farid.m.zakaria@gmail.com>
2025-02-02 20:35:35 -0800

    Site updated at 2025-02-03 04:35:34 UTC

---
+++ projects/raytracer/Refraction_files/Refraction.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.1 local-build-20100721
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('Refraction_files/Crayon_BK_v4%20%2801%29.png'),IWCreateImage('Refraction_files/Crayon_BK_v4%20%2802%29.png'),IWCreateImage('Refraction_files/Crayon_BK_v4%20%2803%29.png'),IWCreateImage('Refraction_files/Crayon_BK_v4%20%2805%29.png'),IWCreateImage('Refraction_files/Crayon_BK_v4%20%2808%29.png'),IWCreateImage('Refraction_files/Crayon_BK_v4%20%2807%29.png'),IWCreateImage('Refraction_files/Crayon_BK_v4%20%2806%29.png'),IWCreateImage('Refraction_files/Crayon_BK_v4%20%2804%29.png')],null,0,0.700000,19.000000,19.000000,19.000000,19.000000,41.000000,41.000000,41.000000,41.000000,2.000000,1.000000,2.000000,1.000000,null,null,null,0.100000)});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Refraction_files/RefractionMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ projects/raytracer/Supersampling_files/Supersampling.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.1 local-build-20100721
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Supersampling_files/SupersamplingMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
