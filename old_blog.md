0c052a8dcab0394853cd60a3ff51e28a087b1faa
Farid Zakaria <farid.m.zakaria@gmail.com>
2025-02-27 16:31:32 -0800

    remove unecessary fies (#18)
    * remove unecessary fies
    * removed javascript highlight.js
    * removed resume
    * resomve css for resume
    * changed index/old_blog html -> md so that they update
    * added corresponding layouts for them
    * Removed projects
    * Increased font of title

---
+++ old_blog.md
@@ -0,0 +1,5 @@
+---
+layout: old_blog
+title: Farid Zakaria's Old Blog
+permalink: /old_blog/
+---
--- projects/raytracer/Scripts/Widgets/HeaderControls/localizedStrings.js
+++
@@ -1,2 +0,0 @@
-
-(function(){var strings={};strings['Subscribe']='Subscribe';strings['Back to Index']='Back to Index';strings['Add Photo']='Add Photo';strings['Play Slideshow']='Play Slideshow';RegisterWidgetStrings("com-apple-iweb-widget-headercontrols",strings);})();
